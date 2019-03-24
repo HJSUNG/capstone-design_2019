6b43f4e9370871ebb6f30a70224310d5a81391cf
HJSUNG <38451225+HJSUNG@users.noreply.github.com>
2019-03-24 21:38:45 +0900

    MainActivity and Graph function

--- application/app/src/main/java/csecau/capstone/capstone02/MainActivity.java
+++ application/app/src/main/java/csecau/capstone/capstone02/MainActivity.java
@@ -3,11 +3,58 @@ package csecau.capstone.capstone02;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        LineChart lineChart = (LineChart) findViewById(R.id.chart);
+
+        ArrayList<Entry> entries = new ArrayList<>();
+        entries.add(new Entry(4f, 0));
+        entries.add(new Entry(8f, 1));
+        entries.add(new Entry(6f, 2));
+        entries.add(new Entry(2f, 3));
+        entries.add(new Entry(18f, 4));
+        entries.add(new Entry(9f, 5));
+        entries.add(new Entry(16f, 6));
+        entries.add(new Entry(5f, 7));
+        entries.add(new Entry(3f, 8));
+        entries.add(new Entry(7f, 10));
+        entries.add(new Entry(9f, 11));
+
+        LineDataSet dataset = new LineDataSet(entries, "# of Calls");
+
+        ArrayList<String> labels = new ArrayList<String>();
+        labels.add("0 am");
+        labels.add("February");
+        labels.add("March");
+        labels.add("6 am");
+        labels.add("May");
+        labels.add("June");
+        labels.add("12 pm");
+        labels.add("August");
+        labels.add("September");
+        labels.add("6 pm");
+        labels.add("November");
+        labels.add("December");
+
+        LineData data = new LineData(labels, dataset);
+        dataset.setColors(ColorTemplate.COLORFUL_COLORS); //
+        /*dataset.setDrawCubic(true); //선 둥글게 만들기
+        dataset.setDrawFilled(true); //그래프 밑부분 색칠*/
+
+        lineChart.setData(data);
+        lineChart.animateY(5000);
     }
 }
