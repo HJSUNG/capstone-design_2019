18b23b90096769740fbb2b001fae2552bde29509
HJSUNG <38451225+HJSUNG@users.noreply.github.com>
2019-03-30 17:26:06 +0900

    new diary activity & buttonlistener

--- application/app/src/main/java/csecau/capstone/capstone02/DiaryActivity.java
+++ application/app/src/main/java/csecau/capstone/capstone02/DiaryActivity.java
@@ -1,16 +1,31 @@
 package csecau.capstone.capstone02;
 
+import android.content.Intent;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ListView;
 
 public class DiaryActivity extends AppCompatActivity {
+
+    private Button newdiaryButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dairy);
+
+        newdiaryButton = (Button)findViewById(R.id.newdiarybutton);
+
+        newdiaryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), NewdiaryActivity.class);
+                startActivity(intent);
+            }
+        });
 
         ListView listview;
         diary_listviewAdapter adapter;
