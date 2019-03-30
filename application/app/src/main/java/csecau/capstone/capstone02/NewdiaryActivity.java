006a9155f1b4c05a94705a851fd96e657d98c421
HJSUNG <38451225+HJSUNG@users.noreply.github.com>
2019-03-30 17:56:51 +0900

    newdiary asynctask

--- application/app/src/main/java/csecau/capstone/capstone02/NewdiaryActivity.java
+++ application/app/src/main/java/csecau/capstone/capstone02/NewdiaryActivity.java
@@ -1,13 +1,112 @@
 package csecau.capstone.capstone02;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class NewdiaryActivity extends AppCompatActivity {
+
+    private Button doneButton;
+    private EditText contentEdittext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_newdiary);
+
+        doneButton = (Button)findViewById(R.id.DoneButton);
+        contentEdittext = (EditText)findViewById(R.id.contentinput);
+
+        doneButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String content = contentEdittext.getText().toString();
+
+                InsertDiary task = new InsertDiary();
+//                task.execute()
+
+            }
+        });
+    }
+
+    class InsertDiary extends AsyncTask<String, Void, String> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+
+            Toast.makeText(NewdiaryActivity.this,result,Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            String ID = (String)params[1];
+            String PW = (String)params[2];
+            String nickname = (String)params[3];
+            String user_type = (String)params[4];
+            String phone = (String)params[5];
+
+            String serverURL = (String)params[0];
+            String postParameters = "ID=" + ID + "&PW=" + PW + "&nickname=" + nickname + "&user_type=" + user_type + "&phone=" + phone;
+
+            try {
+                URL url = new URL(serverURL);
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+
+                httpURLConnection.setReadTimeout(5000);
+                httpURLConnection.setConnectTimeout(5000);
+                httpURLConnection.setRequestMethod("POST");
+                httpURLConnection.connect();
+
+                OutputStream outputStream = httpURLConnection.getOutputStream();
+                outputStream.write(postParameters.getBytes("UTF-8"));
+                outputStream.flush();
+                outputStream.close();
+
+                int responseStatusCode = httpURLConnection.getResponseCode();
+                Log.d("@@@", "POST response code - " + responseStatusCode);
+
+                InputStream inputStream;
+                if(responseStatusCode == HttpURLConnection.HTTP_OK) {
+                    inputStream = httpURLConnection.getInputStream();
+                }
+                else{
+                    inputStream = httpURLConnection.getErrorStream();
+                }
+
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream, "UTF-8");
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+                StringBuilder sb = new StringBuilder();
+                String line = null;
+
+                while((line = bufferedReader.readLine()) != null){
+                    sb.append(line);
+                }
+
+                bufferedReader.close();
+
+                return sb.toString();
+            } catch (Exception e) {
+                return new String("Same ID exists !");
+            }
+        }
     }
 }
