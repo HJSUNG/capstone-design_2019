02c5840530b903754f462d55ce2142463ce8d617
dovicvi <38933683+dovicvi@users.noreply.github.com>
2019-04-27 14:27:54 +0900

    190427_mg

--- application/app/src/main/java/csecau/capstone/capstone02/MedicationActivity.java
+++ application/app/src/main/java/csecau/capstone/capstone02/MedicationActivity.java
@@ -1,16 +1,37 @@
 package csecau.capstone.capstone02;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
 
 public class MedicationActivity extends AppCompatActivity{
+
+    private Button Addbtn, Deletebtn;
+
+//    @Override
+//    public void onClick(View v){
+//        TimePickerFragment mTimePickerFragment = new TimePickerFragment();
+//        mTimePickerFragment.show(getSupportFragmentManager(), TimePickerFragment.FRAGMENT_TAG);
+//    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_medication);
 
+        Addbtn = (Button)findViewById(R.id.btn_add);
+
+        Addbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TimePickerFragment mTimePickerFragment = new TimePickerFragment();
+                mTimePickerFragment.show(getSupportFragmentManager(), TimePickerFragment.FRAGMENT_TAG);
+            }
+        });
     }
 }
