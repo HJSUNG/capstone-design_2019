26f6d22ed24988f00a84f70bb3955cfbd57e1de6
dovicvi <38933683+dovicvi@users.noreply.github.com>
2019-06-02 06:51:39 +0900

    그래프 조정

--- application/app/src/main/java/csecau/capstone/capstone02/MainActivity.java
+++ application/app/src/main/java/csecau/capstone/capstone02/MainActivity.java
@@ -42,10 +42,6 @@ import java.net.URL;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    Context context;
-    ArrayList<Integer> jsonList = new ArrayList<>();
-    ArrayList<String> labelList = new ArrayList<>();
-
     private String[] glucose_list;
 
 
@@ -57,9 +53,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        context=this.getBaseContext();
-//        DataVolley();
 
         glucoseButton = (Button)findViewById(R.id.GlucoseButton);
         medicationButton = (Button)findViewById(R.id.MedicationButton);
@@ -125,136 +118,38 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
-        alarm_listviewAdapter adapter;
+//        alarm_listviewAdapter adapter;
+//
+//        adapter = new alarm_listviewAdapter();
+//        main_alarm.setAdapter(adapter);
 
-        adapter = new alarm_listviewAdapter();
-        main_alarm.setAdapter(adapter);
 
 
-
-        LineChart lineChart = (LineChart) findViewById(R.id.chart);
-
-        ArrayList<Entry> entries = new ArrayList<>();
-        for(int i=0;i<glucose_list.length;i++){
-
-        }
+//        LineChart lineChart = (LineChart) findViewById(R.id.chart);
+//        ArrayList<Entry> entries = new ArrayList<>();
 
 
 //        entries.add(new Entry(100, 2));
-//        entries.add(new Entry(123, 3));
-//        entries.add(new Entry(100, 25));
-//        entries.add(new Entry(200, 26));
-//        entries.add(new Entry(180, 29));
-//        entries.add(new Entry(123, 30));
-//        entries.add(new Entry(100, 32));
-//        entries.add(new Entry(120, 33));
-//        entries.add(new Entry(120, 34));
-//        entries.add(new Entry(90, 36));
-//        entries.add(new Entry(120, 81 ));
 
-        LineDataSet dataset = new LineDataSet(entries, "Blood Glucose");
-        dataset.setColor(Color.parseColor("#FF0000"));
-        dataset.setCircleColor(R.color.black);
-        dataset.setCircleColorHole(R.color.black);
+//        LineDataSet dataset = new LineDataSet(entries, "Blood Glucose");
+//        dataset.setColor(Color.parseColor("#FF0000"));
+//        dataset.setCircleColor(R.color.black);
+//        dataset.setCircleColorHole(R.color.black);
 
 
-        ArrayList<String> labels = new ArrayList<String>();
+//        ArrayList<String> labels = new ArrayList<String>();
 //        labels.add("04-07 6am");
-//        labels.add("04-07 12am");
-//        labels.add("04-07 6pm");
-//        labels.add("04-07 12pm");
-//        labels.add("04-08 6am");
-//        labels.add("04-08 12am");
-//        labels.add("04-08 6pm");
-//        labels.add("04-08 12pm");
-//        labels.add("04-09 6am");
-//        labels.add("04-09 12am");
-//        labels.add("04-09 6pm");
-//        labels.add("04-09 12pm");
-//        labels.add("04-10 6am");
-//        labels.add("04-10 12am");
-//        labels.add("04-10 6pm");
-//        labels.add("04-10 12pm");
-//        labels.add("04-11 6am");
-//        labels.add("04-11 12am");
-//        labels.add("04-11 6pm");
-//        labels.add("04-11 12pm");
-//        labels.add("04-12 6am");
-//        labels.add("04-12 12am");
-//        labels.add("04-12 6pm");
-//        labels.add("04-12 12pm");
-//        labels.add("04-13 6am");
-//        labels.add("04-13 12am");
-//        labels.add("04-13 6pm");
-//        labels.add("04-13 12pm");
-//        labels.add("04-14 6am");
-//        labels.add("04-14 12am");
-//        labels.add("04-14 6pm");
-//        labels.add("04-14 12pm");
-//        labels.add("04-15 6am");
-//        labels.add("04-15 12am");
-//        labels.add("04-15 6pm");
-//        labels.add("04-15 12pm");
-//        labels.add("04-16 6am");
-//        labels.add("04-16 12am");
-//        labels.add("04-16 6pm");
-//        labels.add("04-16 12pm");
-//        labels.add("04-17 6am");
-//        labels.add("04-17 12am");
-//        labels.add("04-17 6pm");
-//        labels.add("04-17 12pm");
-//        labels.add("04-18 6am");
-//        labels.add("04-18 12am");
-//        labels.add("04-18 6pm");
-//        labels.add("04-18 12pm");
-//        labels.add("04-19 6am");
-//        labels.add("04-19 12am");
-//        labels.add("04-19 6pm");
-//        labels.add("04-19 12pm");
-//        labels.add("04-20 6am");
-//        labels.add("04-20 12am");
-//        labels.add("04-20 6pm");
-//        labels.add("04-20 12pm");
-//        labels.add("04-21 6am");
-//        labels.add("04-21 12am");
-//        labels.add("04-21 6pm");
-//        labels.add("04-21 12pm");
-//        labels.add("04-22 6am");
-//        labels.add("04-22 12am");
-//        labels.add("04-22 6pm");
-//        labels.add("04-22 12pm");
-//        labels.add("04-23 6am");
-//        labels.add("04-23 12am");
-//        labels.add("04-23 6pm");
-//        labels.add("04-23 12pm");
-//        labels.add("04-24 6am");
-//        labels.add("04-24 12am");
-//        labels.add("04-24 6pm");
-//        labels.add("04-24 12pm");
-//        labels.add("04-25 6am");
-//        labels.add("04-25 12am");
-//        labels.add("04-25 6pm");
-//        labels.add("04-25 12pm");
-//        labels.add("04-26 6am");
-//        labels.add("04-26 12am");
-//        labels.add("04-26 6pm");
-//        labels.add("04-26 12pm");
-//        labels.add("04-27 6am");
-//        labels.add("04-27 12am");
-//        labels.add("04-27 6pm");
-//        labels.add("04-27 12pm");
 
-
-        LineData data = new LineData(labels, dataset);
-//        dataset.setColors(ColorTemplate.COLORFUL_COLORS); //
+//        LineData data = new LineData(labels, dataset);
+//        dataset.setColors(ColorTemplate.COLORFUL_COLORS);
         /*dataset.setDrawCubic(true); //선 둥글게 만들기
         dataset.setDrawFilled(true); //그래프 밑부분 색칠*/
 
-        lineChart.setData(data);
-        lineChart.animateY(5000);
+//        lineChart.setData(data);
+//        lineChart.animateY(5000);
 
-        MainActivity.Getglucoselist getglucoselist = new Getglucoselist();
-        getglucoselist.execute("http://capstone02.cafe24.com/retrieve_glucose.php", user_id);
+        Getglucoselist getglucoselist = new Getglucoselist();
+        getglucoselist.execute("http://capstone02.cafe24.com/retrieve_glucose_graph.php", user_id);
     }
 
     class Getglucoselist extends AsyncTask<String, Void, String> {
@@ -270,15 +165,41 @@ public class MainActivity extends AppCompatActivity {
             String result_string = result;
             glucose_list = result_string.split("<br>");
 
+            ListView listview;
+            glucose_listviewAdapter adapter;
+
+            adapter = new glucose_listviewAdapter();
+
+//            listview = (ListView) findViewById(R.id.glucoselistview);
+//            listview.setAdapter(adapter);
+            LineChart lineChart = (LineChart) findViewById(R.id.chart);
+            ArrayList<Entry> entries = new ArrayList<>();
+            ArrayList<String> labels = new ArrayList<String>();
+//            ArrayList date_hour= new ArrayList();
+
             String test_sentence = glucose_list[0];
             boolean test_contains = result.contains("comma");
-
+            int i=0;
             if (result.contains("<comma>")) {
                 for (String glucose : glucose_list) {
                     String glucose_split[] = glucose.split("<comma>");
-//                    entries.add(glucose_split[1], glucose_split[0]);
+//                    adapter.addItem(glucose_split[1], glucose_split[2]);
+                    String date = glucose_split[1].substring(5,13);
+                    entries.add(new Entry(Integer.parseInt(glucose_split[2]), i));
+                    labels.add(date);
+//                    date_hour.add(date);
+                    i++;
                 }
             }
+            LineDataSet dataset = new LineDataSet(entries, "Blood Glucose");
+            dataset.setColor(Color.parseColor("#FF0000"));
+            dataset.setCircleColor(R.color.black);
+            dataset.setCircleColorHole(R.color.black);
+
+            LineData data = new LineData(labels, dataset);
+
+            lineChart.setData(data);
+            lineChart.animateY(5000);
         }
 
         @Override
@@ -332,6 +253,20 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     }
+
+//    private void setChart(List<Record> records){
+//        LineChart lineChart = binding.lineChart;
+//        lineChart.invalidate();
+//        lineChart.clear();
+//
+//        ArrayList<Entry> values = new ArrayList<>();
+//
+//        for(Record record: records){
+//            long dateTime = record.getDateTime();
+//            float weight = (float) record.getValue();
+//            values.add(new Entry(dateTime,value));
+//        }
+//    }
 
 
 }
